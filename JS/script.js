0f8735ffd45a26bdff43bd5133f33072482dc3b1
badilleins <badilleins@gmail.com>
2023-05-11 16:10:02 -0500

    Cursos

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-$ ('.clasess .nav-link[category="all"]')
- $('.nav-link').click(function(){
-var catproduct = $(this).attr('category');
-console.log(catproduct);
-$('.categorias_item').hide();
-$('.categorias_item[category="'+catproduct+'"').show();
- 
+const boton1 = document.getElementById('c');
+const boton2 = document.getElementById('r');
+const boton3 = document.getElementById('p');
+const div1 = document.getElementById('cyber');
+const div2 = document.getElementById('redes');
+const div3 = document.getElementById('Progra');
+div1.style.display='none'
+div2.style.display = 'none';
+div3.style.display = 'none';
+
+boton1.addEventListener('click', () => {
+    div1.style.display='inline'
+  div2.style.display = 'none';
+  div3.style.display = 'none';
 });
-$('.nav-link[category="all"]').click(function(){
-    $('.categorias_item').show();
+
+boton2.addEventListener('click', () => {
+    div2.style.display='inline'
+    div1.style.display = 'none';
+   div3.style.display = 'none';
 });
-});
+boton3.addEventListener('click', () => {
+    div3.style.display='inline'
+    div1.style.display = 'none';
+    div2.style.display = 'none';
+  });
