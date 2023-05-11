9fe1b5aa980505d196bb1eef2378f58f52ef0a8d
xLexus <alexponluisa123@gmail.com>
2023-05-11 16:00:35 -0500

    Actualizacion

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-$ ('.clasess .nav-link[category="all"]')
- $('.nav-link').click(function(){
-var catproduct = $(this).attr('category');
-console.log(catproduct);
-$('.categorias_item').hide();
-$('.categorias_item[category="'+catproduct+'"').show();
- 
-});
-$('.nav-link[category="all"]').click(function(){
-    $('.categorias_item').show();
-});
+$(document).ready(function () {
+    $('.clasess .nav-link[category="all"]')
+    $('.nav-link').click(function () {
+        var catproduct = $(this).attr('category');
+        console.log(catproduct);
+        $('.categorias_item').hide();
+        $('.categorias_item[category="' + catproduct + '"').show();
+
+    });
+    $('.nav-link[category="all"]').click(function () {
+        $('.categorias_item').show();
+    });
 });
